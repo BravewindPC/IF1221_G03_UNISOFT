112a7c121e94f38f3831b62ca4d33761c67fd6f1
Bayu Palamarta Wirawan <bayupalamartawirawan@gmail.com>
2026-05-09 21:16:34 +0700

    Another Fixing to the Wild Cards Picking

--- src/file1.pl
+++ src/file1.pl
@@ -2,8 +2,8 @@ kartu([0,1,2,3,4,5,6,7,8,9,'skip','reverse','drawtwo','wild','wilddrawfour'], ['
 get([Element|_], 0, Element).
 get([_|Tail], Index, Element) :- Index > 0, NI is Index - 1, get(Tail, NI, Element).
 
-ambil_kartu_acak(X, Y):- random(0, 15, P), kartu(A, B), get(A, P, X), X = 'wild', !, Y = 'hitam'.
-ambil_kartu_acak(X, Y):- random(0, 15, P), kartu(A, B), get(A, P, X), X = 'wilddrawfour', !, Y = 'hitam'.
+ambil_kartu_acak(X, Y):- random(0, 15, P), kartu(A, _), get(A, P, X), X = 'wild', !, Y = 'hitam'.
+ambil_kartu_acak(X, Y):- random(0, 15, P), kartu(A, _), get(A, P, X), X = 'wilddrawfour', !, Y = 'hitam'.
 ambil_kartu_acak(X, Y) :- random(0, 15, P), random(0, 4, Q), kartu(A, B), get(A, P, X), get(B, Q, Y).
 
 ambil_kartu_top(X,Y) :- random(0, 10, P), random(0, 4, Q), kartu(A, B), get(A, P, X), get(B, Q, Y).
